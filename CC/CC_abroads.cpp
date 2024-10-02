b29598b1606e358c58119ec7cf2320cdca1e5b55
hamm-bit <namiicoo@yahoo.com>
2024-10-02 16:34:30 +1000

    W4 sync, A done

--- CC/CC_abroads.cpp
+++ CC/CC_abroads.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <queue>
+#include <utility>
 using namespace std;
 
 #define ll long long
@@ -6,20 +8,20 @@ using namespace std;
 
 // Edit based on given Qs
 const int nMOD = (int) 1e9 + 7;
-const ll MAX = 1e5 + 5;
+const ll MAX = 5e5 + 5;
 
 // vector<int> DP(MAX);
 // vector<vector<int>> DP(MAX, vector<int> MAX);
 
-void arr_print(vector<int> z) {
-    for (int i : z) {
-        cout << i << " ";
-    } cout << "\n";
-    return;
+void arr_print(queue<ll> z, int q = 0) {
+    for (;!z.empty(); z.pop()) {
+        cout << z.front() << "\n";
+    } return;
 }
 
 // Credit to Neal Wu's blog (https://codeforces.com/blog/entry/62393)                                                   
 // And Sebastiano Vigna for authoring splitmix64
+/*
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
         // http://xorshift.di.unimi.it/splitmix64.c
@@ -34,157 +36,38 @@ struct custom_hash {
         return splitmix64(x + FIXED_RANDOM);
     }
 };
-
-// Change for suitability
-/*
-int b_search(int i) {
-    ll l = 0ll, r = DP.size();
-    ll mid = l + (r - l) / 2, old_mid = -1ll;
-    while (l <= r) {
-        if (DP[mid] == i || mid == old_mid)
-            break;
-        else if (DP[mid] > i)
-            r = mid;
-        else
-            l = mid;
-    }
-    return mid;
-}
 */
 
 // =============================================
 // ======== Global variables begin here ========
 // =============================================
 
-struct S_que {
-    ll init = 0;
-    ll operator()(ll a, ll b) { return a + b; }
-};
+int n, m, q, a, k, x, y;
+vector<array<int, 3>> rds;
+array<vector<int>, MAX> adj;
+array<int, MAX> ranks{}, pops{}, Ks, As, xs;
+vector<pair<int, ll>> parents;
+priority_queue<ll> un_sum;
 
-struct M_que {
-    ll init = LONG_LONG_MIN;
-    ll operator()(ll a, ll b) { return max(a, b); }
-};
-
-struct B_que {
-    bool init = true;
-    bool operator()(bool a, bool b) { return a && b; }
-};
-
-
-ll n, m, D, d, x, y;
-array<int, MAX> parents{}, ranks{};
-array<ll, MAX> Ds{};
-
-// =============================================
+// =================n============================
 // ======== Custom functions begin here ========
 // =============================================
 
-// ====================================
-// ======== BEGIN SEGMENT TREE ========
-// ====================================
-
-// q_t must
-//  - have default constructor (should be automatically created)
-//  - be callable as q(val_t, val_t) -> val_t
-//  - have `init` member of val_t (should lose to all other values when q()
-// is applied)
-template <typename val_t, typename q_t, ll max_size>
-struct RT {
-    val_t rtree[4 * max_size];
-    ll _size;
-    q_t q;
-
-    RT(ll size) {
-        _size = size;
-        q = q_t();
-        for (ll i = 0; i++ < _size;)
-            update(i, q.init);
-    }
-    
-    void update(ll pos, val_t value) {
-        r_upd(pos, value, 0, _size);
-    }
-
-    val_t query(ll left, ll right) {
-        return r_que(left, right, 0, _size);
-    }
-
-    // DEBUG OUTPUT
-    friend ostream &operator<<(ostream &os, const RT &rt) {
-        rt.debug(os, 0, rt._size);
-        return os;
-    }
-
-    private:
-    // Recursively update the tree by binary divide segments
-    // NOTE: DEPENDENT ON `q` AND CAN FAIL, VARY BY SITUATIONS
-    void r_upd(ll p, val_t v, ll cL, ll cR, ll i = 1) {
-        if (cR - cL == 1) {
-            rtree[i] = v;
-            return;
-        }
-        ll mid = (cL + cR) / 2;
-        if (p < mid)    r_upd(p, v, cL, mid, i * 2);
-        else            r_upd(p, v, mid, cR, i * 2 + 1);
-        rtree[i] = q(rtree[i * 2], rtree[i * 2 + 1]);
-    }
-
-
-    // Recursively query the tree by binary divide segments
-    // NOTE: DEPENDENT ON `q` AND CAN FAIL, VARY BY SITUATIONS
-    val_t r_que(ll qL, ll qR, ll cL, ll cR, ll i = 1) {
-        if (cL == qL && cR == qR)   return rtree[i];
-        ll mid = (cL + cR) / 2;
-        val_t ans = q.init;
-        if (qL < mid)
-            ans = q(ans, r_que(qL, min(qR, mid), cL, mid, i * 2));
-        if (qR > mid)
-            ans = q(ans, r_que(max(qL, mid), qR, mid, cR, i * 2 + 1));
-        return ans;
-    }
-
-
-    // DEFAULT TO BEING BARRED FROM BEING CALLED DIRECTLY
-    // Vary config and pub/priv based on situations
-    void debug(ostream &os, ll cL, ll cR, ll i = 1, ll h = 0) const {
-        for (ll i = 0; i < h; i++)
-            os << "  ";
-        os << "[" << cL << "," << cR << ") = ";
-        if (rtree[i] == q.init)     os << "X";
-        else                        os << rtree[i];
-        os << "\n";
-        if (cR - cL > 1) {
-            ll mid = (cL + cR) / 2;
-            debug(os, cL, mid, i * 2, h + 1);
-            debug(os, mid, cR, i * 2 + 1, h + 1);
-        }
-    }
-};
-
-// ==================================
-// ======== END SEGMENT TREE ========
-// ==================================
-
-// ==================================
-// ======== BEGIN UNION FIND ========
-// ==================================
 
 /*
 * Create new sets (leaves)
 */
 void make_set(int v) {
-    parents[v] = v;
-    ranks[v] = 0;
+    parents[v] = make_pair(v, pops[v]);
 }
 
 /*
 * UF find a parent, rank increases for every recursion 
 */
 int find_set(int v) {
-    if (v == parents[v])
+    if (v == parents[v].first)
         return v;
-    return find_set(parents[v]);
+    return find_set(parents[v].first);
 }
 
 /*
@@ -197,7 +80,8 @@ void union_sets(int a, int b) {
     if (a != b) {
         if (ranks[a] < ranks[b])
             swap(a, b);
-        parents[b] = a;
+        parents[b].first = a;
+        parents[b].second += parents[a].second; 
         if (ranks[a] == ranks[b])
             ranks[a]++;
     }
@@ -209,11 +93,89 @@ void union_sets(int a, int b) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    char op;
+    queue<ll> ans;
 
     // ======== Main begins here ========
-    cin >> n >> m;
+    cin >> n >> m >> q;
+    for (int i = 0; i++ < n;)
+        cin >> pops[i];
+    cout << "Output ended\n";
 
-    RT<ll, M_que, MAX> max_tree(n);
+    for (int i = 0; i++ < m;) {
+        cin >> x >> y;
+        array<int, 3> in{{x, y, 0}};
+        rds.push_back(in);
+    }
+
+    cout << "Output ended\n";
+
+    // Convert the queries to be in reverse order
+    for (int i = 0; i++ < q;) {
+        if (op == 'D') {
+            cin >> k, Ks[i] = k;
+            rds[k][2] = 1;
+        } else if (op == 'P') {
+            cin >> As[i] >> xs[i];
+            pops[As[i]] = xs[i];
+        } else {
+            // debug op
+        }
+    }
+
+    for (int i = 0; i++ < m;) {
+        if (rds[i][2]) continue;
+        adj[rds[i][0]].push_back(rds[i][1]);
+        adj[rds[i][1]].push_back(rds[i][0]);
+    }
+
+    // First BFS to find all regions' pops
+    array<bool, MAX> gvisited{};
+    queue<int> tobe;
+    for (int j = 1; j++ < q;) {
+        if (gvisited[j])
+            continue;
+
+        array<bool, MAX> visited{};
+        make_set(j), tobe.push(j);
+        gvisited[j] = 1, visited[j] = 1;
+
+        while (!tobe.empty()) {
+            int curr = tobe.front();
+            tobe.pop();
+            // cout << curr << " ";
+
+            for (int x : adj[j]) {
+                if (!visited[x]) {
+                    visited[x] = 1, tobe.push(x);
+                    union_sets(j, x);
+                }
+            }
+        }
+        // cout << curr << "\n";
+        // Index maxes of unions into an array, join and 
+        int un_max = find_set(j);
+        un_sum.push(parents[un_max].second);
+    }
+
+    for (int j = q; j > 1; j--) {
+        ans.push(un_sum.top());
+        if (op == 'D') {
+            x = rds[Ks[j]][0], y = rds[Ks[j]][1];
+            x = find_set(x), y = find_set(y);
+            // CHECK THAT THESE ARE IN SYNC
+            union_sets(x, y);
+            un_sum.push(parents[x].second);
+            un_sum.push(parents[y].second);
+        } else if (op == 'P') {
+            a = As[j], k = find_set(a);
+            // CHECK THAT THESE ARE IN SYNC
+            parents[k].second += xs[j] - pops[a];
+            un_sum.push(parents[k].second);
+        }
+    }
+    
+    arr_print(ans);
 
     return 0;
 }
---
+++ CC/CC_abroads.py
@@ -0,0 +1,7 @@
+import math
+import queue
+
+nMOD = 1e9 + 7;
+nMAX = 5e5 + 5;
+
+
